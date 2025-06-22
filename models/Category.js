4010d695ac1f414de11a5779be20dd89a2a82bb4
TarunPatidar04 <tarun.coderfarm@gmail.com>
2025-06-22 18:22:59 +0530

    Add models for Category, Comment, News, and User with schemas and pre-save hooks

---
+++ models/Category.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const slugify = require("slugify");
+
+const categorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  slug: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  timestamps: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+categorySchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+module.exports = mongoose.model("Category", categorySchema);
---
+++ models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+  fullname: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ["admin", "author"],
+    default: "author",
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 12);
+  }
+  next();
+});
+
+module.exports = mongoose.model("User", userSchema);
